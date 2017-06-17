e9536d3d8f63c6111c9257b35377899abc562071
akhilvenkateswaran <akhil.venkateswaran@gmail.com>
2017-06-17 21:49:54 +0530

    Sms Receiver modified

--- app/src/main/java/com/example/dell/otpdemo/MainActivity.java
+++ app/src/main/java/com/example/dell/otpdemo/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity
     {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sms_receiver);
         OtpNumber = (TextView) findViewById(R.id.otpTextView);
         Intent intent = getIntent();
         String message = intent.getStringExtra("OTP");
